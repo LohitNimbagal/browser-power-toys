c4e22fb6d53d0b447e693b39bd92753b1bbe4a5a
LohitNimbagal <nimbagallohit@gmail.com>
2024-10-27 18:52:19 +0530

    add google site verification to head tag

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="Z7M9AegeY80jKee3FZtL3PT216TJtY3Obf87L8vH0Yo" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
