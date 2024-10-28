68d86c026de79ad7f33298b3373ca42a8d68b738
LohitNimbagal <nimbagallohit@gmail.com>
2024-10-29 00:57:52 +0530

    update hero section and tools section

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Montserrat, Roboto } from 'next/font/google'
 import "./globals.css";
+import { ThemeProvider } from "@/components/there-provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const montserrat = Montserrat({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  variable: '--montserrat',
+  subsets: ['latin']
+})
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700'],
+  variable: '--roboto',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -32,9 +34,14 @@ export default function RootLayout({
         <meta name="google-site-verification" content="Z7M9AegeY80jKee3FZtL3PT216TJtY3Obf87L8vH0Yo" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} ${montserrat.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
